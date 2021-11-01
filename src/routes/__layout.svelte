18537ad408f58275b6f178ab52a2967e7aa753d8
rcastellotti <me@rcastellotti.dev>
2021-11-02 00:50:05 +0100

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		font-family: 'Electronic Highway Sign' !important;
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,14 @@
+<div class="relative">
+	<img class="static" src="static/cartello_autostrada.svg" alt="sign" />
+	<div class="absolute text text-4xl text-yellow-400 ">
+		un morto su 10 per avere letto ma anche divano questo cartello
+	</div>
+</div>
+
+<style>
+	.text {
+		left: 40%;
+		right: 15%;
+		top: 35%;
+	}
+</style>
