31a3125a8d3134b73f59b7c777d52e482a0ee3c3
rcastellotti <me@rcastellotti.dev>
2021-11-03 16:55:35 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,30 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
+import path from 'path';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		vite: {
+			resolve: {
+				alias: {
+					$components: path.resolve('./src/components')
+				}
+			}
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;
