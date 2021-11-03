7000e6c5661e4591b40991fbee758fe057a35eab
rcastellotti <me@rcastellotti.dev>
2021-11-03 17:27:27 +0100

    net

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,7 @@ import path from 'path';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+		adapter: adapter(),
 		vite: {
 			resolve: {
 				alias: {
